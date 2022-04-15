ae82d54a0e92357a6e91492ccbd96b8cd23800ee
Killian Schneider <80042061+yoschigamer@users.noreply.github.com>
2022-04-15 15:50:56 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php require("actions/Users/SecurityAction.php"); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <a href="actions/Users/LogoutAction.php" style="margin: 10px; color: rgb(0, 128, 255); text-decoration: none;">logout ?</a>
+
+</body>
+
+</html>
