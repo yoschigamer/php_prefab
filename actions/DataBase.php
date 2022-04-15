f902a5fbd08c9356458c356c3f146a7f00ce7e76
Killian Schneider <80042061+yoschigamer@users.noreply.github.com>
2022-04-15 15:51:52 +0200

    Update DataBase.php

--- actions/DataBase.php
+++ actions/DataBase.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 try {
-    $bdd = new PDO('mysql:host=localhost;dbname=first_form;charset=utf8', 'root', '');
+    $bdd = new PDO('mysql:host=localhost;dbname=NOM_DE_LA_BDD;charset=utf8', 'root', '');
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
 } catch (Exception $e) {
